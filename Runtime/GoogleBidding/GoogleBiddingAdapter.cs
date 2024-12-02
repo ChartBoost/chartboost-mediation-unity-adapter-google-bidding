3389ca1b1a455172255df2d3ae56832a21387dc4
helium-service <helium-service@chartboost.com>
2024-12-02 19:24:48 +0000

    Syncing changes from private repository

--- Runtime/GoogleBidding/GoogleBiddingAdapter.cs
+++ Runtime/GoogleBidding/GoogleBiddingAdapter.cs
@@ -13,7 +13,7 @@ namespace Chartboost.Mediation.GoogleBidding
         /// <summary>
         /// The partner adapter Unity version.
         /// </summary>
-        public const string AdapterUnityVersion = "5.0.1";
+        public const string AdapterUnityVersion = "5.0.5";
         
         /// <inheritdoc cref="IPartnerAdapterConfiguration.AdapterNativeVersion"/>
         public static string AdapterNativeVersion => Instance.AdapterNativeVersion;
